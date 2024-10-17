a942835a0ad8d508cceb474b21129a371e798f9a
ortega <kabweortega@gmail.com>
2024-10-17 20:16:24 +0200

    Setup and configure user microservice

---
+++ user/src/test/java/com/ortega/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
