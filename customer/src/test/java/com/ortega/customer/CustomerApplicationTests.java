154d12e75d14737a08a3d66dd99fa62491a8fa1e
ortega <kabweortega@gmail.com>
2024-10-19 12:58:17 +0200

    Setup and configure customer service

---
+++ customer/src/test/java/com/ortega/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
